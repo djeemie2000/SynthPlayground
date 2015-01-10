0fd13a2cdcbac435db738fa6eeeb1449c7b40746
djeemie2000 <peter.jamee@telenet.be>
2015-01-10 14:51:44 +0100

    CmdStackItem: more convenient initialisation

--- CommandStackLib/CommandStack.cpp
+++ CommandStackLib/CommandStack.cpp
@@ -29,3 +29,65 @@ SCmdStackItem::SCmdStackItem(const std::string& Name, bool BoolValue, int IntVal
     , s_HasTimeStamp(true)
 {
 }
+
+SCmdStackItem::SCmdStackItem(const string &Name)
+    : s_Name(Name)
+    // other members use member value initialisation
+{
+}
+
+SCmdStackItem &SCmdStackItem::Name(const string &Name)
+{
+    s_Name = Name;
+    return *this;
+}
+
+SCmdStackItem &SCmdStackItem::BoolValue(bool Value)
+{
+    s_BoolValue = Value;
+    return *this;
+}
+
+SCmdStackItem &SCmdStackItem::IntValue(int Value)
+{
+    s_IntValue = Value;
+    return *this;
+}
+
+SCmdStackItem &SCmdStackItem::FloatValue(float Value)
+{
+    s_FloatValue = Value;
+    return *this;
+}
+
+SCmdStackItem &SCmdStackItem::TimeStamp(std::uint32_t TimeStamp)
+{
+    s_TimeStamp = TimeStamp;
+    s_HasTimeStamp = true;
+    return *this;
+}
+
+const string &SCmdStackItem::Name() const
+{
+    return s_Name;
+}
+
+bool SCmdStackItem::BoolValue() const
+{
+    return s_BoolValue;
+}
+
+int SCmdStackItem::IntValue() const
+{
+    return s_IntValue;
+}
+
+float SCmdStackItem::FloatValue() const
+{
+    return s_FloatValue;
+}
+
+std::uint32_t SCmdStackItem::TimeStamp() const
+{
+    return s_TimeStamp;
+}
--- CommandStackLib/CommandStack.h
+++ CommandStackLib/CommandStack.h
@@ -11,17 +11,30 @@ using namespace std;
 
 struct SCmdStackItem
 {
-    std::string s_Name;
-    bool s_BoolValue;
-    int s_IntValue;
-    float s_FloatValue;
-    std::uint32_t s_TimeStamp;
-    bool s_HasTimeStamp;
+    std::string s_Name{};
+    bool s_BoolValue{false};
+    int s_IntValue{-1};
+    float s_FloatValue{-1.0f};
+    std::uint32_t s_TimeStamp{0};
+    bool s_HasTimeStamp{false};
 
     SCmdStackItem();
 
     SCmdStackItem(const std::string& Name, bool BoolValue, int IntValue, float FloatValue);
     SCmdStackItem(const std::string& Name, bool BoolValue, int IntValue, float FloatValue, std::uint32_t TimeStamp);
+    explicit SCmdStackItem(const std::string& Name);
+
+    SCmdStackItem &Name(const std::string& Name);
+    SCmdStackItem &BoolValue(bool Value);
+    SCmdStackItem &IntValue(int Value);
+    SCmdStackItem &FloatValue(float Value);
+    SCmdStackItem &TimeStamp(std::uint32_t TimeStamp);
+
+    const std::string& Name() const;
+    bool BoolValue() const;
+    int IntValue() const;
+    float FloatValue() const;
+    std::uint32_t TimeStamp() const;
 };
 
 typedef std::deque<SCmdStackItem> CmdStack;
