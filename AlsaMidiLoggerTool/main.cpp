6028ab0121c0a37eb87a459ebbd7d91cfbefa9e0
djeemie2000 <peter.jamee@telenet.be>
2015-04-27 13:13:03 +0200

    AlsaMidiLoggerTool: ctrl-C support #189

--- AlsaMidiLoggerTool/main.cpp
+++ AlsaMidiLoggerTool/main.cpp
@@ -2,10 +2,18 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#include <csignal>
 #include "AlsaMidiInput.h"
 #include "LogMidiInputHandler.h"
 
 using namespace std;
+
+volatile std::sig_atomic_t gSignalStatus = 0;
+
+void signal_handler(int Signal)
+{
+    gSignalStatus = Signal;
+}
 
 void About()
 {
@@ -18,14 +26,28 @@ int main()
 {
     About();
 
+    // setup
     CLogMidiInputHandler Handler;
     CAlsaMidiInput MidiInput(Handler);
 
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGTERM, signal_handler);
+    std::signal(SIGILL, signal_handler);
+    std::signal(SIGSEGV, signal_handler);
+    std::signal(SIGFPE, signal_handler);
+    std::signal(SIGABRT, signal_handler);
+
+    // Run
     MidiInput.Open("AlsaMidiLoggerTool", "MidiIn");
 
-    // wait untill keypressed
-    char bs = std::getchar();
+    // detect ctrl-C or other signals
+    while(gSignalStatus==0)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    std::cout << "Captured signal " << gSignalStatus << std::endl;
 
+    // cleanup
     MidiInput.Close();
 
     About();
