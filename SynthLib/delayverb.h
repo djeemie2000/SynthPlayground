d07453fc42ec4acdf17240a05d9d53d445f69e71
djeemie2000 <peter.jamee@telenet.be>
2016-03-06 15:36:36 +0100

    Delay reverb: add offset to delay to 'randomize' different delays.

--- SynthLib/delayverb.h
+++ SynthLib/delayverb.h
@@ -10,7 +10,7 @@ class CDelayVerb
 public:
     CDelayVerb(int Capacity)
         : m_Capacity(Capacity)
-        , m_MaxDelay(m_Capacity/19)
+        , m_MaxDelay(m_Capacity/19-DelayOffset)
         , m_DelayLine(Capacity, 0)
         , m_DampFilter()
     {
@@ -29,14 +29,14 @@ public:
         }
 
         // read delayed values
-        T ReadValue = m_DelayLine.Read(Delay*2)/4
-                    + m_DelayLine.Read(Delay*3)
-                    + m_DelayLine.Read(Delay*5)
-                    + m_DelayLine.Read(Delay*7)
-                    + m_DelayLine.Read(Delay*11)
-                    + m_DelayLine.Read(Delay*13)/2
-                    + m_DelayLine.Read(Delay*17)/2
-                    + m_DelayLine.Read(Delay*19)/2;
+        T ReadValue = m_DelayLine.Read(DelayOffset+Delay*2)/4
+                    + m_DelayLine.Read(DelayOffset+Delay*3)
+                    + m_DelayLine.Read(DelayOffset+Delay*5)
+                    + m_DelayLine.Read(DelayOffset+Delay*7)
+                    + m_DelayLine.Read(DelayOffset+Delay*11)
+                    + m_DelayLine.Read(DelayOffset+Delay*13)/2
+                    + m_DelayLine.Read(DelayOffset+Delay*17)/2
+                    + m_DelayLine.Read(DelayOffset+Delay*19)/2;
         ReadValue *= Normalize;
 
         T WriteValue = In + Feedback*m_DampFilter(ReadValue, Damp);
@@ -49,10 +49,12 @@ private:
     static constexpr int NumDelays = 8;
 //    static const int m_DelayMultiplier[NumDelays] = { 2, 3, 5, 7, 11, 13, 17, 19};
     static constexpr T Normalize = T(1)/NumDelays;
+    static constexpr int DelayOffset = 100;
     const int m_Capacity;
     const int m_MaxDelay;
     CDelayLine2<T> m_DelayLine;
     COnePoleLowPassFilter<T> m_DampFilter;
+    //COnePoleHighPassFilter<T> m_DampFilter;
 };
 
 #endif // DELAYVERB_H
